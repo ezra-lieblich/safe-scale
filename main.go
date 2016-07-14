ed235e2f59e2a03c4802a58bf361e1f0e7b03a52
ezra-lieblich <elieblich@pivotal.io>
2016-07-14 11:21:01 -0400

    renamed the file names. Updated the metadata. Cleaned and refactored code to look cleaner

---
+++ main.go
@@ -0,0 +1,318 @@
+package main
+
+import (
+	"github.com/cloudfoundry/cli/plugin"
+	"fmt"
+	"net/http"
+	"time"
+	"flag"
+	"errors"
+)
+
+type SafeScaler struct {
+	blue          *AppProp
+	green         *AppProp
+	green_routes  []Route
+	blue_routes   []Route
+	services      []string
+	trans         string
+	test          string
+	inst          string
+	timeout       int
+	original_name string
+	space         string
+	client        *http.Client
+}
+type AppProp struct {
+	name   string
+	routes []Route
+	alive  bool
+}
+type Route struct {
+	host   string
+	domain string
+}
+
+func (c *SafeScaler) Run(cliConnection plugin.CliConnection, args []string) {
+	if err := c.getArgs(args); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := c.getApp(cliConnection, args[1]); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := c.createNewApp(cliConnection); err != nil {
+		fmt.Println(err)
+		return
+	}
+	c.client = http.DefaultClient //client for endpoint monitoring
+	if healthy := c.healthTest(c.client); !healthy {
+		fmt.Println("new app is not healthy. Can not continue blue-green deployment. Routes from old app will not be transferred to new app")
+		return
+	}
+	if err := c.mapping(cliConnection); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := c.unmapping(cliConnection); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := c.monitorTransactions(c.client); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if err := c.powerDown(cliConnection); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
+
+func (c *SafeScaler) GetMetadata() plugin.PluginMetadata {
+	return plugin.PluginMetadata{
+		Name: "SafeScaler",
+		Version: plugin.VersionType{
+			Major: 1,
+			Minor: 0,
+			Build: 0,
+		},
+		MinCliVersion: plugin.VersionType{
+			Major: 1,
+			Minor: 0,
+			Build: 0,
+		},
+		Commands: []plugin.Command{
+			{
+				Name: "safe-scale",
+				HelpText: "safely scales down your application using blue green deployment",
+				UsageDetails: plugin.Usage{
+					Usage: "safe-scale\n	cf safe-scale app_name [--inst] [--trans] [--test] [--timeout]",
+					Options: map[string]string{
+						"-inst":        "number of instances for new app",
+						"-trans":        "endpoint to monitor transaction",
+						"-test":        "endpoint to test if new app is healthy",
+						"-timeout":        "time in seconds to monitor transactions",
+					},
+				},
+			},
+		},
+	}
+}
+
+func (c *SafeScaler) getApp(cliConnection plugin.CliConnection, name string) error {
+	//getting app properties
+	app, err := cliConnection.GetApp(name)
+	c.services = []string{}
+	if err != nil {
+		return err
+	}
+	properties := &AppProp{
+		name:        "",
+		routes:        []Route{},
+		alive:        true,
+	}
+	properties.name = app.Name
+	//getting routes from app
+	for _, value := range app.Routes {
+		new_route := Route{
+			domain:        value.Domain.Name,
+			host:        value.Host,
+		}
+		properties.routes = append(properties.routes, new_route)
+	}
+	//getting services from app
+	for _, value := range app.Services {
+		c.services = append(c.services, value.Name)
+	}
+	if err = c.getSpace(cliConnection); err != nil {
+		return err
+	}
+	c.blue = properties
+	c.blue_routes = c.blue.routes
+	c.green = &AppProp{name:"", routes: []Route{}, alive: false}
+	return nil
+}
+
+func (c *SafeScaler) addMap(cliConnection plugin.CliConnection, app *AppProp, route Route) error {
+	if _, err := cliConnection.CliCommand("map-route", app.name, route.domain, "--hostname", route.host); err != nil {
+		return err
+	}
+	app.routes = append(app.routes, route)
+	return nil
+}
+func (c *SafeScaler) removeMap(cliConnection plugin.CliConnection, app *AppProp, route Route) error {
+	if _, err := cliConnection.CliCommand("unmap-route", app.name, route.domain, "--hostname", route.host);
+	err != nil {
+		return err
+	}
+	//updating app routes array
+	for i, value := range app.routes {
+		if value.host == route.host && value.domain == route.domain {
+			new_routes := append(app.routes[:i], app.routes[i + 1:]...)
+			app.routes = new_routes
+			return nil
+		}
+	}
+	return errors.New("could not find map to unmap")
+
+}
+
+func (c *SafeScaler) healthTest(client *http.Client) bool {
+	//no endpoint so just continue with deployment
+	if c.test == "" {
+		return true
+	}
+	endpoint := "https://" + c.green.routes[0].host + "." + c.green.routes[0].domain + c.test
+	result, err := client.Get(endpoint) //test endpoint
+	//not ok or error so test failed 300 multiple things going on
+	if result.StatusCode != 200 || err != nil {
+		return false
+	}
+	return true
+
+}
+func (c *SafeScaler) monitorTransactions(client *http.Client) error {
+	//no endpoint so just regular blue green deployment
+	if c.trans == "" {
+		return nil
+	}
+	trans_endpoint := "https://" + c.blue.routes[0].host + "." + c.blue.routes[0].domain + c.trans
+	base := time.Now() //baseline time to measure against
+	current := time.Since(base).Seconds()
+	//loop to continuously monitor transactions until it times out
+	for current < float64(c.timeout) {
+		result, err := client.Get(trans_endpoint)
+		if err != nil {
+			return err
+		}
+		//no content so there are no more transactions have parameter that has utc time
+		if result.StatusCode == 204 {
+			return nil
+		}
+		if result.StatusCode != 200 {
+			return errors.New("Status code is not okay. Check to make sure the app is healthy")
+		}
+		time.Sleep(3 * time.Second)
+		current = time.Since(base).Seconds()
+	}
+	return errors.New("The request timed out. Can not safely shut down the old app.")
+}
+
+func (c *SafeScaler) getArgs(args []string) error {
+	if len(args) == 1 {
+		return errors.New("Insufficient arguments. Did not specify an app")
+	}
+	c.original_name = args[1]
+	f := flag.NewFlagSet("f", flag.ContinueOnError)
+	inst_ptr := f.String("inst", "1", "the number of instances for new app")
+	trans_ptr := f.String("trans", "", "endpoint path to monitor transactions")
+	test_ptr := f.String("test", "", "endpoint path to test new app deployed")
+	timeout_ptr := f.Int("timeout", 120, "time in seconds before transaction monitoring times out")
+	//Do not want to parse through the command name and app name. Just focused on flags
+	f.Parse(args[2:])
+	c.inst = *inst_ptr
+	c.test = *test_ptr
+	c.trans = *trans_ptr
+	c.timeout = *timeout_ptr
+	return nil
+}
+
+func (c *SafeScaler) getSpace(cliConnection plugin.CliConnection) error {
+	space, err := cliConnection.GetCurrentSpace()
+	if err != nil {
+		return err
+	}
+	c.space = space.Name
+	return nil
+}
+func (c *SafeScaler) createNewApp(cliConnection plugin.CliConnection) error {
+	if err := c.pushApp(cliConnection); err != nil {
+		return err
+	}
+	//need to bind all the services of blue app to the green app
+	for _, val := range c.services {
+		if err := c.bindService(cliConnection, val); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+func (c *SafeScaler) pushApp(cliConnection plugin.CliConnection) error {
+	new_name := "new-" + c.original_name
+	domain := c.blue.routes[0].domain
+	if _, err := cliConnection.CliCommand("push", new_name, "-i", c.inst, "--hostname", new_name, "-d", domain); err != nil {
+		return err
+	}
+	c.green.name = new_name
+	c.green.routes = append(c.green.routes, Route{host: new_name, domain: domain})
+	c.green.alive = true
+	return nil
+
+}
+func (c *SafeScaler) bindService(cliConnection plugin.CliConnection, val string) error {
+	if _, err := cliConnection.CliCommand("bind-service", c.green.name, val); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *SafeScaler) mapping(cliConnection plugin.CliConnection) error {
+	//creates a temp route for old app
+	temp_route, err := c.createRoute(cliConnection)
+	if err != nil {
+		return err
+	}
+	//add temp route to old app
+	if err = c.addMap(cliConnection, c.blue, temp_route); err != nil {
+		return err
+	}
+	//add all routes from old app to new app
+	for _, val := range c.blue_routes {
+		if err = c.addMap(cliConnection, c.green, val); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *SafeScaler) unmapping(cliConnection plugin.CliConnection) error {
+	//unmap all routes from blue with exception of temp route for monitoring purposes
+	for _, val := range c.blue_routes {
+		if err := c.removeMap(cliConnection, c.blue, val); err != nil {
+			return err
+		}
+	}
+	//remove the first route that was pushed with new app
+	if err := c.removeMap(cliConnection, c.green, c.green.routes[0]); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (c *SafeScaler) createRoute(cliConnection plugin.CliConnection) (Route, error) {
+	temp_route := Route{
+		domain: c.blue.routes[0].domain,
+		host: "temp" + c.blue.routes[0].host,
+	}
+	if _, err := cliConnection.CliCommand("create-route", c.space, temp_route.domain, "--hostname", temp_route.host);
+	err != nil {
+		return temp_route, err
+	}
+	return temp_route, nil
+}
+func (c *SafeScaler) powerDown(cliConnection plugin.CliConnection) error {
+	if err := c.removeMap(cliConnection, c.blue, c.blue.routes[0]); err != nil {
+		return err
+	}
+	if _, err := cliConnection.CliCommand("stop", c.blue.name); err != nil {
+		return err
+	}
+	c.blue.alive = false
+	return nil
+}
+func main() {
+	plugin.Start(new(SafeScaler))
+}
--- main_test.go
+++ main_test.go
@@ -1,19 +1,20 @@
 package main
 
-import (//HTTP versus HTTPS
+import (
+	//HTTP versus HTTPS???
 	"github.com/cloudfoundry/cli/plugin/models"
 	"github.com/cloudfoundry/cli/plugin/pluginfakes"
-	."github.com/onsi/ginkgo"
-	."github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"errors"
 	"github.com/nicholasf/fakepoint"
 )
-var _ = Describe("safescale", func() {
-	var(
-		connection    	*pluginfakes.FakeCliConnection
-		ExamplePlugin   *SafeScaler
-		result 		*AppProp
 
+var _ = Describe("safescale", func() {
+	var (
+		connection      *pluginfakes.FakeCliConnection
+		ExamplePlugin   *SafeScaler
+		result          *AppProp
 	)
 	BeforeEach(func() {
 		connection = &pluginfakes.FakeCliConnection{}
@@ -21,11 +22,11 @@ var _ = Describe("safescale", func() {
 	})
 	Describe("get arguments", func() {
 		It("should fail when insufficient amount of arguments", func() {
-			err:= ExamplePlugin.getArgs([]string{"safe-scale"})
-			Expect(err.Error()).To(Equal("Did not specify an app"))
+			err := ExamplePlugin.getArgs([]string{"safe-scale"})
+			Expect(err.Error()).To(Equal("Insufficient arguments. Did not specify an app"))
 		})
 		It("should set to default values", func() {
-			err:= ExamplePlugin.getArgs([]string{"safe-scale", "test-app"})
+			err := ExamplePlugin.getArgs([]string{"safe-scale", "test-app"})
 			Expect(err).To(BeNil())
 			Expect(ExamplePlugin.original_name).To(Equal("test-app"))
 			Expect(ExamplePlugin.inst).To(Equal("1"))
@@ -33,9 +34,9 @@ var _ = Describe("safescale", func() {
 			Expect(ExamplePlugin.test).To(Equal(""))
 			Expect(ExamplePlugin.timeout).To(Equal(120))
 		})
-		It("should set all flags sucessfully", func(){
+		It("should set all flags sucessfully", func() {
 			args := []string{"safe-scale", "foo", "--inst", "4", "--test", "/test", "-trans", "/trans", "--timeout", "40"}
-			err:= ExamplePlugin.getArgs(args)
+			err := ExamplePlugin.getArgs(args)
 			Expect(err).To(BeNil())
 			Expect(ExamplePlugin.original_name).To(Equal("foo"))
 			Expect(ExamplePlugin.inst).To(Equal("4"))
@@ -45,7 +46,7 @@ var _ = Describe("safescale", func() {
 		})
 		It("should set some flags and leave others as default", func() {
 			args := []string{"safe-scale", "bar", "--trans", "/trans", "-test", "/test"}
-			err:= ExamplePlugin.getArgs(args)
+			err := ExamplePlugin.getArgs(args)
 			Expect(err).To(BeNil())
 			Expect(ExamplePlugin.original_name).To(Equal("bar"))
 			Expect(ExamplePlugin.inst).To(Equal("1"))
@@ -54,26 +55,31 @@ var _ = Describe("safescale", func() {
 		})
 	})
 	Describe("app properties", func() {
-		It("app  exists", func(){
-			app := plugin_models.GetAppModel{Name: "green-app"}
-			connection.GetAppReturns(app, nil)
-			result, _ = ExamplePlugin.getApp(connection, "green-app")
-			Expect(result.name).To(Equal("green-app"))
+		BeforeEach(func() {
+			ExamplePlugin.blue = &AppProp{name:"", routes: []Route{}, alive: false}
 		})
-		It("app doesn't exist", func(){
+		It("app  exists", func() {
+			app := plugin_models.GetAppModel{Name: "blue-app"}
+			connection.GetAppReturns(app, nil)
+			err := ExamplePlugin.getApp(connection, "blue-app")
+			Expect(err).To(BeNil())
+			Expect(ExamplePlugin.blue.name).To(Equal("blue-app"))
+		})
+		It("app doesn't exist", func() {
 			app := plugin_models.GetAppModel{Name: "blue-app"}
 			connection.GetAppReturns(app, errors.New("The app doesn't exist"))
-			_, err := ExamplePlugin.getApp(connection, "blue-app")
+			err := ExamplePlugin.getApp(connection, "blue-app")
 			Expect(err.Error()).To(Equal("The app doesn't exist"))
 
 		})
 		It("has no routes", func() {
 			app := plugin_models.GetAppModel{}
 			connection.GetAppReturns(app, nil)
-			result, _ = ExamplePlugin.getApp(connection, "") //name is not relevant
-			Expect(result.routes).To(Equal([]Route{}))
+			err := ExamplePlugin.getApp(connection, "") //name is not relevant
+			Expect(err).To(BeNil())
+			Expect(ExamplePlugin.blue.routes).To(Equal([]Route{}))
 		})
-		It("has one route", func(){
+		It("has one route", func() {
 			domain_name := plugin_models.GetApp_DomainFields{Name: "cfapps.io"}
 			route := []plugin_models.GetApp_RouteSummary{
 				{
@@ -83,53 +89,57 @@ var _ = Describe("safescale", func() {
 			}
 			app := plugin_models.GetAppModel{Routes: route}
 			connection.GetAppReturns(app, nil)
-			result, _ = ExamplePlugin.getApp(connection, "")
-			Expect(result.routes).To(Equal([]Route{{host: "trial", domain: "cfapps.io"}}))
+			err := ExamplePlugin.getApp(connection, "")
+			Expect(err).To(BeNil())
+			Expect(ExamplePlugin.blue.routes).To(Equal([]Route{{host: "trial", domain: "cfapps.io"}}))
 		})
-		It("has more than one route", func(){
+		It("has more than one route", func() {
 			domain_name := plugin_models.GetApp_DomainFields{Name: "cfapps.io"}
 			route := []plugin_models.GetApp_RouteSummary{
 				{
-					Host: 	"foo",
-					Domain:	domain_name,
+					Host:        "foo",
+					Domain:        domain_name,
 				},
 				{
-					Host:	"bar",
+					Host:        "bar",
 					Domain: domain_name,
 				},
 			}
 			app := plugin_models.GetAppModel{Routes: route}
 			connection.GetAppReturns(app, nil)
-			result, _ = ExamplePlugin.getApp(connection, "")
-			Expect(result.routes).To(Equal([]Route{
+			err := ExamplePlugin.getApp(connection, "")
+			Expect(err).To(BeNil())
+			Expect(ExamplePlugin.blue.routes).To(Equal([]Route{
 				{host: "foo", domain: "cfapps.io"}, {host: "bar", domain: "cfapps.io"}}))
 		})
 		It("has no services", func() {
 			app := plugin_models.GetAppModel{}
 			connection.GetAppReturns(app, nil)
-			result, _ = ExamplePlugin.getApp(connection, "")
+			err := ExamplePlugin.getApp(connection, "")
+			Expect(err).To(BeNil())
 			Expect(ExamplePlugin.services).To(Equal([]string{}))
 		})
 		It("has multiple services", func() {
-			services:= []plugin_models.GetApp_ServiceSummary{
+			services := []plugin_models.GetApp_ServiceSummary{
 				{Name: "foo"},
 				{Name: "bar"},
 			}
-			app:= plugin_models.GetAppModel{Services: services}
+			app := plugin_models.GetAppModel{Services: services}
 			connection.GetAppReturns(app, nil)
-			result, _ = ExamplePlugin.getApp(connection, "")
+			err := ExamplePlugin.getApp(connection, "")
+			Expect(err).To(BeNil())
 			Expect(ExamplePlugin.services).To(Equal([]string{"foo", "bar"}))
 		})
 	})
 	Describe("create new app", func() {
 		BeforeEach(func() {
-			ExamplePlugin.original_name= "test-app"
+			ExamplePlugin.original_name = "test-app"
 			ExamplePlugin.green = &AppProp{name:"", routes: []Route{}, alive: false}
 			ExamplePlugin.blue = &AppProp{routes: []Route{{domain:"cfapps.io"}}}
 		})
 		It("should push a new app sucesfully", func() {
 			connection.CliCommandReturns([]string{"yes"}, nil)
-			err:=ExamplePlugin.pushApp(connection)
+			err := ExamplePlugin.pushApp(connection)
 			Expect(err).To(BeNil())
 			Expect(ExamplePlugin.green.name).To(Equal("new-test-app"))
 			Expect(ExamplePlugin.green.routes).To(Equal([]Route{{domain: "cfapps.io", host: "new-test-app"}}))
@@ -137,17 +147,17 @@ var _ = Describe("safescale", func() {
 		})
 		It("should fail if it can't push the app", func() {
 			connection.CliCommandReturns(nil, errors.New("failed to push the app"))
-			err:= ExamplePlugin.pushApp(connection)
+			err := ExamplePlugin.pushApp(connection)
 			Expect(err.Error()).To(Equal("failed to push the app"))
 		})
 		It("should bind a service sucesfully", func() {
 			connection.CliCommandReturns([]string{"sucess"}, nil)
-			err:= ExamplePlugin.bindService(connection, "foo-db")
+			err := ExamplePlugin.bindService(connection, "foo-db")
 			Expect(err).To(BeNil())
 		})
 		It("should fail if it can't bind a service to an app", func() {
 			connection.CliCommandReturns(nil, errors.New("failed to bind service"))
-			err:= ExamplePlugin.bindService(connection, "bad-service")
+			err := ExamplePlugin.bindService(connection, "bad-service")
 			Expect(err.Error()).To(Equal("failed to bind service"))
 		})
 	})
@@ -158,7 +168,7 @@ var _ = Describe("safescale", func() {
 			Expect(err.Error()).To(Equal("there is no space"))
 		})
 		It("should fail if it can't get space", func() {
-			space_field:= plugin_models.SpaceFields{Name: "sandbox"}
+			space_field := plugin_models.SpaceFields{Name: "sandbox"}
 			connection.GetCurrentSpaceReturns(plugin_models.Space{space_field}, nil)
 			err := ExamplePlugin.getSpace(connection)
 			Expect(err).To(BeNil())
@@ -171,17 +181,17 @@ var _ = Describe("safescale", func() {
 			ExamplePlugin.test = "/test"
 		})
 		It("should return true if the app is healthy", func() {
-			maker:= fakepoint.NewFakepointMaker()
-			maker.NewGet("https://foo.cfapps.io/test",200)
-			client:=maker.Client()
-			result:= ExamplePlugin.healthTest(client)
+			maker := fakepoint.NewFakepointMaker()
+			maker.NewGet("https://foo.cfapps.io/test", 200)
+			client := maker.Client()
+			result := ExamplePlugin.healthTest(client)
 			Expect(result).To(BeTrue())
 		})
 		It("should return false if the app is not healthy", func() {
-			maker:= fakepoint.NewFakepointMaker()
-			maker.NewGet("https://foo.cfapps.io/test",400)
-			client:= maker.Client()
-			result:= ExamplePlugin.healthTest(client)
+			maker := fakepoint.NewFakepointMaker()
+			maker.NewGet("https://foo.cfapps.io/test", 400)
+			client := maker.Client()
+			result := ExamplePlugin.healthTest(client)
 			Expect(result).To(BeFalse())
 		})
 	})
@@ -192,97 +202,97 @@ var _ = Describe("safescale", func() {
 			ExamplePlugin.timeout = 4
 		})
 		It("should fail when it times out", func() {
-			maker:= fakepoint.NewFakepointMaker()
+			maker := fakepoint.NewFakepointMaker()
 			maker.NewGet("https://bar.cfapps.io/trans", 200).Duplicate(100)
-			client:= maker.Client()
-			result:= ExamplePlugin.monitorTransactions(client)
+			client := maker.Client()
+			result := ExamplePlugin.monitorTransactions(client)
 			Expect(result.Error()).To(Equal("The request timed out. Can not safely shut down the old app."))
 		})
 		It("should pass when transactions are empty initially", func() {
-			maker:= fakepoint.NewFakepointMaker()
+			maker := fakepoint.NewFakepointMaker()
 			maker.NewGet("https://bar.cfapps.io/trans", 204).Duplicate(100)
-			client:= maker.Client()
-			result:= ExamplePlugin.monitorTransactions(client)
+			client := maker.Client()
+			result := ExamplePlugin.monitorTransactions(client)
 			Expect(result).To(BeNil())
 		})
 		It("should fail when get request is bad", func() {
-			maker:= fakepoint.NewFakepointMaker()
+			maker := fakepoint.NewFakepointMaker()
 			maker.NewGet("https://bar.cfapps.io/trans", 200) // will go from 200 to 404 after 1 call
-			client:= maker.Client()
-			result:= ExamplePlugin.monitorTransactions(client)
+			client := maker.Client()
+			result := ExamplePlugin.monitorTransactions(client)
 			Expect(result.Error()).To(Equal("Status code is not okay. Check to make sure the app is healthy"))
 		})
 	})
 	Describe("mapping", func() {
-		BeforeEach(func(){
+		BeforeEach(func() {
 			app1 := &AppProp{
-				name: 	"green-app",
+				name:        "green-app",
 				routes: []Route{{host: "temp", domain: "cfapps.io"}},
 			}
 			ExamplePlugin.green = app1
 		})
 
-		It("should add a map sucessfully", func(){
-			connection.CliCommandReturns([]string{"it worked"},nil)
+		It("should add a map sucessfully", func() {
+			connection.CliCommandReturns([]string{"it worked"}, nil)
 			moved := Route{host: "moved", domain:"cfapps.io"}
-			err := ExamplePlugin.addMap(connection,ExamplePlugin.green, moved)
+			err := ExamplePlugin.addMap(connection, ExamplePlugin.green, moved)
 			Expect(err).To(BeNil())
 			Expect(ExamplePlugin.green.routes).To(Equal([]Route{{host: "temp", domain: "cfapps.io"},
-								{host: "moved", domain: "cfapps.io"}}))
+				{host: "moved", domain: "cfapps.io"}}))
 		})
-		It("should fail if it can't add map", func(){
+		It("should fail if it can't add map", func() {
 			connection.CliCommandReturns(nil, errors.New("could not map route"))
 			bad_route := Route{host: "fake", domain: "cfapps.io"}
-			err:= ExamplePlugin.addMap(connection, ExamplePlugin.green, bad_route)
+			err := ExamplePlugin.addMap(connection, ExamplePlugin.green, bad_route)
 			Expect(err.Error()).To(Equal("could not map route"))
 			Expect(ExamplePlugin.green.routes).To(Equal([]Route{{host: "temp", domain:"cfapps.io"}}))
 		})
 
 	})
 	Describe("unmapping", func() {
-		BeforeEach(func(){
+		BeforeEach(func() {
 			app := &AppProp{
-				name:	"foo",
+				name:        "foo",
 				routes: []Route{{host: "foo", domain:"cfapps.io"}, {host: "bar", domain: "cfapps.io"}},
 			}
 			result = app
 		})
 		It("should unmap a route from app sucessfully", func() {
-			deleted_route:= Route{host: "bar", domain: "cfapps.io"}
+			deleted_route := Route{host: "bar", domain: "cfapps.io"}
 			connection.CliCommandReturns([]string{"it worked"}, nil)
-			err:= ExamplePlugin.removeMap(connection, result, deleted_route)
+			err := ExamplePlugin.removeMap(connection, result, deleted_route)
 			Expect(err).To(BeNil())
 			Expect(result.routes).To(Equal([]Route{{host: "foo", domain: "cfapps.io"}}))
 		})
 		It("should unmap a route from app to an empty route", func() {
 			app1 := &AppProp{
-				name:	"foo",
+				name:        "foo",
 				routes: []Route{{host: "bar", domain: "cfapps.io"}},
 			}
 			result = app1
-			deleted_route:= Route{host: "bar", domain: "cfapps.io"}
+			deleted_route := Route{host: "bar", domain: "cfapps.io"}
 			connection.CliCommandReturns([]string{"it worked"}, nil)
-			err:= ExamplePlugin.removeMap(connection, result, deleted_route)
+			err := ExamplePlugin.removeMap(connection, result, deleted_route)
 			Expect(err).To(BeNil())
 			Expect(result.routes).To(Equal([]Route{}))
 		})
 		It("should fail if it can't unmap a route from app sucesfully", func() {
-			bad_route:= Route{host: "bad", domain: "cfapps.io"} //route doesn't exist
+			bad_route := Route{host: "bad", domain: "cfapps.io"} //route doesn't exist
 			connection.CliCommandReturns(nil, errors.New("could not unmap route"))
-			err:= ExamplePlugin.removeMap(connection, result, bad_route)
+			err := ExamplePlugin.removeMap(connection, result, bad_route)
 			Expect(err.Error()).To(Equal("could not unmap route"))
 			Expect(result.routes).To(Equal([]Route{{host: "foo", domain:"cfapps.io"},
-										{host: "bar", domain: "cfapps.io"}}))
+				{host: "bar", domain: "cfapps.io"}}))
 		})
 
 	})
 	Describe("power down", func() {
 		BeforeEach(func() {
-			ExamplePlugin.blue= &AppProp{alive: true, routes: []Route{{domain: "cfapps.io", host: "foo"}}}
+			ExamplePlugin.blue = &AppProp{alive: true, routes: []Route{{domain: "cfapps.io", host: "foo"}}}
 		})
 		It("should power down sucessfully", func() {
 			connection.CliCommandReturns([]string{"sucess"}, nil)
-			err:= ExamplePlugin.powerDown(connection)
+			err := ExamplePlugin.powerDown(connection)
 			Expect(err).To(BeNil())
 			Expect(ExamplePlugin.blue.alive).To(BeFalse())
 		})
